88f704bd1e6b468a7e2b400bfe4a5f68ff4a75c7
DogsofCPP <147498298+DogsofCPP@users.noreply.github.com>
2024-07-09 14:46:27 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "mainwindow.h"
+#include<QMainWindow>
+#include <QApplication>
+#include<iostream>
+#include<QtDebug>
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    qDebug()<<"www";
+    return a.exec();
+}
